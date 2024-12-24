6bc2d9476a5cd4b5fd9a38b6b6e8ed977e889c08
MahdiyDev <mahdiydev@gmail.com>
2024-12-24 12:16:06 +0500

    added: factorial example

--- functions_and_stack/build.sh
+++ functions_and_stack/build.sh
@@ -8,3 +8,7 @@ MINGW_LIB="C:\Program Files\mingw\mingw32\i686-w64-mingw32\lib"
 as power.s -o power.o --32
 
 ld power.o -o power.exe -L "$MINGW_LIB" -lkernel32 -mi386pe
+
+as factorial.s -o factorial.o --32
+
+ld factorial.o -o factorial.exe -L "$MINGW_LIB" -lkernel32 -mi386pe
